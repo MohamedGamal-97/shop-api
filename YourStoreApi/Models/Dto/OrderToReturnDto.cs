3ac95b8dc77753c1f606793ddab7fde89617c12b
mohamed gamal <midogemy100@gmail.com>
2022-05-24 20:16:07 +0200

    before fix

--- YourStoreApi/Models/Dto/OrderToReturnDto.cs
+++ YourStoreApi/Models/Dto/OrderToReturnDto.cs
@@ -7,7 +7,8 @@ namespace YourStoreApi.Models.Dto
         public int Id { get; set; }
         public string BuyerEmail { get; set; }
         public DateTimeOffset OrderDate { get; set; }
-        public Address ShipToAddress { get; set; }
+        public OderAggregate.Address ShipToAddress { get; set; }
+
         public string DeliveryMethod { get; set; }
         public decimal ShippingPrice { get; set; }
         public IReadOnlyList<OrderItemDto> OrderItems { get; set; }
